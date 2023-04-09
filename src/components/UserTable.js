f8a28db857e0eef50f20b81ec93a8530b383cc84
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-04-09 09:17:24 +0530

    Update UserTable.js

--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -1,5 +1,42 @@
-import React, { useState, useEffect } from "react";
+UserTable.js
+import React, { useEffect, useState } from "react";
 
-function UserTable() {}
+function UserTable() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await response.json();
+      setUsers(data.sort((a, b) => a.id - b.id));
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>Website</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.phone}</td>
+            <td>{user.website}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
 
 export default UserTable;
