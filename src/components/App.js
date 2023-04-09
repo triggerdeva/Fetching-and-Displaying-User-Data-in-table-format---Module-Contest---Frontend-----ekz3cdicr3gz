0dcbfc84d6ff86c05e73e7c106acb2241d8222c9
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-04-09 09:16:44 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/App.css";
+import UserTable from "./UserTable";
+const App = () => {
+	return (
+		<div id="main">
+			<UserTable />;
+		</div>
+	);
+};
+
+export default App;
